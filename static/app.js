287cfa69ec8932f23475ee9e380d49a7d94b1149
Alex Chan <alex@alexwlchan.net>
2025-01-25 18:30:02 +0000

    Add some code for getting fic info

--- static/app.js
+++ static/app.js
@@ -18,6 +18,65 @@ function findContentOpfPath(zip) {
 
 
 
+/* Given the contents of the `content.opf` file, return certain key
+ * information like the title, author, and fandom. */
+function getFicInfo(xml) {
+  const parser = new DOMParser();
+  const xmlDoc = parser.parseFromString(xml, "text/xml");
+  
+  const namespaceResolver = (prefix) =>
+    prefix === 'dc' ? 'http://purl.org/dc/elements/1.1/' : null;
+  
+  const STRING_TYPE = 2;  // XPathResult.STRING_TYPE
+  
+  // The title of the fic will be in a <dc:title> node, for example:
+  //
+  //     <dc:title>Operation Cameo</dc:title>
+  //
+  const title = xmlDoc.evaluate(
+    './/dc:title',
+    xmlDoc,
+    namespaceResolver,
+    STRING_TYPE,
+    null
+  ).stringValue;
+  
+  // The author of the fic will be in a <dc:author> node, for example:
+  //
+  //     <dc:creator …>alexwlchan</dc:creator>
+  //
+  const author = xmlDoc.evaluate(
+    './/dc:creator',
+    xmlDoc,
+    namespaceResolver,
+    STRING_TYPE,
+    null
+  ).stringValue;
+  
+  // The fandom is the *third* of the <dc:subject> nodes, for example:
+  //
+  //    <dc:subject>Fanworks</dc:subject>
+  //    <dc:subject>General Audiences</dc:subject>
+  //    <dc:subject>Operation Mincemeat: A New Musical - SpitLip</dc:subject>
+  //
+  // The first subject is always "Fanworks", and it looks like the rating
+  // is always the second subject.
+  //
+  // See comment in the AO3 codebase here:
+  // https://github.com/otwcode/otwarchive/blob/2362dd772452a71dda9c62409b935f275a0d131f/app/models/download_writer.rb#L162-L164
+  const fandom = xmlDoc.evaluate(
+    './/dc:subject[3]',
+    xmlDoc,
+    namespaceResolver,
+    STRING_TYPE,
+    null
+  ).stringValue;
+  
+  return { title, author, fandom };
+}
+
+
+
 if (typeof module !== 'undefined') {
-  module.exports = { findContentOpfPath };
+  module.exports = { findContentOpfPath, getFicInfo };
 }
